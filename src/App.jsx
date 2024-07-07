82145f7711c967e01ea699c31b5e17cfdc50fd40
Aathish <152775496+aathi-aathi@users.noreply.github.com>
2024-07-07 13:53:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import {createContext, useState } from 'react'
+import './App.css'
+import Product from './product'
+import data from './product.json'
+const productContext=createContext(null);
+function App() {
+  const [products,setProducts]=useState(data)
+ function increaseQuantity(index){
+    setProducts(products =>
+      products.map((item, i) =>
+          i === index ? { ...item, quantity: item.quantity + 1 } : item
+      )
+  );
+  }
+  function decreaseQuantity(index){
+    setProducts(products =>
+      products.map((item, i) =>
+          i === index && item.quantity>1 ? { ...item,  quantity: item.quantity - 1 } : item
+      )
+  );
+  }
+ return (
+    <productContext.Provider value={{products,increaseQuantity,decreaseQuantity,setProducts}}>
+      <nav ><h1>Shopping Cart</h1></nav> 
+       <div className='products'>
+      <Product/>
+      </div>
+    </productContext.Provider>
+  )
+}
+export default App
+export {productContext}
